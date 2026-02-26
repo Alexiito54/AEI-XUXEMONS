eea8ec69d839939734439865dbce4d92089da590
ivimolinaa <ivimolinaaguayo@alumnes.ilerna.com>
2026-02-26 19:32:19 +0100

    Inicio pagina usuarios

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register';
+import { PagInfoUsuario } from './pag-info-usuario/pag-info-usuario';
 
 export const routes: Routes = [
   // Ruta principal '/' → redirige a register
@@ -7,7 +8,10 @@ export const routes: Routes = [
   
   // Ruta register
   { path: 'register', component: RegisterComponent },
-  
+
+ // Ruta info-usuario
+  { path: 'info-usuario', component: PagInfoUsuario },
+
   // Cualquier otra → register
   { path: '**', redirectTo: 'register', pathMatch: 'full' }
 ];
---
+++ frontend/src/app/pag-info-usuario/pag-info-usuario.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pag-info-usuario',
+  imports: [],
+  templateUrl: './pag-info-usuario.html',
+  styleUrl: './pag-info-usuario.css',
+})
+export class PagInfoUsuario {
+
+}
