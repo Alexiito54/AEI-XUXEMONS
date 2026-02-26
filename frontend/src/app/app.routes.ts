17604e6f30a7b118c925b712b5a2cae11939ff36
Alexiito54 <afrancesretamero50@alumnes.ilerna.com>
2026-02-26 16:40:58 +0100

    Cambios Frontend, estilos y cambios de errores del menu de registros

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Ruta principal '/' → redirige a register
+  { path: '', redirectTo: '/register', pathMatch: 'full' },
+  
+  // Ruta register
+  { path: 'register', component: RegisterComponent },
+  
+  // Cualquier otra → register
+  { path: '**', redirectTo: 'register', pathMatch: 'full' }
+];
