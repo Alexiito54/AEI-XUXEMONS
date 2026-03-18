1f0331bcb1cb9fc631a0df5cfff2f8c5161832a2
Erlzo <ericromerogasol53@gmail.com>
2026-03-18 16:39:11 +0100

    Navegación

--- frontend/src/app/pag-info-usuario/pag-info-usuario.ts
+++ frontend/src/app/pag-info-usuario/pag-info-usuario.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pag-info-usuario',
@@ -7,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrl: './pag-info-usuario.css',
 })
 export class PagInfoUsuario {
+  constructor(private router: Router) {}
 
+  navegarAlInicio() {
+    this.router.navigate(['/pagina-principal']);
+  }
+
+  navegarAlXuxedex() {
+    this.router.navigate(['/xuxedex']);
+  }
+
+  navegarAlInventario() {
+    this.router.navigate(['/mochila']);
+  }
+
+  navegarAlAmigos() {
+    this.router.navigate(['/amigos']);
+  }
+
+  navegarAlBatalla() {
+    this.router.navigate(['/batalla']);
+  }
+
+  navegarAlChat() {
+    this.router.navigate(['/chat']);
+  }
+
+  navegarAlPerfil() {
+    this.router.navigate(['/info-usuario']);
+  }
+  
+  navegarAlAdmin() {
+    this.router.navigate(['/admin']);
+  }
 }
