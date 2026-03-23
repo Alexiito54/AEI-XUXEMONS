69ddbc737ddd53ab9517bee285e2ba18a38cf515
Alexiito54 <afrancesretamero50@alumnes.ilerna.com>
2026-03-23 19:23:04 +0100

    Cambios rutas protegidas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,23 +4,18 @@ import { PaginaPrincipal } from './pagina-principal/pagina-principal';
 import { LoginComponent } from './login/login';
 import { PagInfoUsuario } from './pag-info-usuario/pag-info-usuario';
 import { MochilaComponent } from './mochila/mochila.component';
+import { authGuard } from './guards/auth.guard'; // ← añade esto
 
 export const routes: Routes = [
   { path: '', redirectTo: '/register', pathMatch: 'full' },
   
   { path: 'login',    component: LoginComponent },
-  
   { path: 'register', component: RegisterComponent },
 
-  // Ruta para la página principal
-  { path: 'pagina-principal', component: PaginaPrincipal },
-
- // Ruta info-usuario
-  { path: 'info-usuario', component: PagInfoUsuario },
-
-  { path: 'mochila', component: MochilaComponent },
-
-  // Cualquier otra → register
+  // Rutas protegidas 
+  { path: 'pagina-principal', component: PaginaPrincipal, canActivate: [authGuard] },
+  { path: 'info-usuario',     component: PagInfoUsuario,  canActivate: [authGuard] },
+  { path: 'mochila',          component: MochilaComponent, canActivate: [authGuard] },
 
   { path: '**', redirectTo: 'register', pathMatch: 'full' }
 ];
